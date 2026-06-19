4bb6f761a5d6f3a10c0515d21a771343a8e2088f
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-06-19 10:22:12 -0600

    feat: connect real data to dashboard

--- frontendSodaHodgson/src/pages/Dashboard.jsx
+++ frontendSodaHodgson/src/pages/Dashboard.jsx
@@ -1,8 +1,25 @@
 import DashboardHeader from "../components/layout/dashboard/DashboardHeader";
 import StatCard from "../components/layout/dashboard/StatCard";
 import QuickActions from "../components/layout/dashboard/QuickActions";
+import { useCashSessions } from "../hooks/useCashSession";
+import { useSales } from "../hooks/useSales";
+import { useTables } from "../hooks/useTables";
+import {useProducts} from "../hooks/useProducts";
 
 export default function Dashboard() {
+  const { data: cashSessions = [] } = useCashSessions();
+  const { data: sales = [] } = useSales();
+  const { data: initialTables = [] } = useTables();
+  const { data: products = [] } = useProducts();
+
+  const ingresos = cashSessions.reduce(
+    (acc, item) => acc + item.opening_balance,
+    0
+  );
+  const facturas= sales.length;
+  const tables= initialTables.length;
+  const initialProducts= products.length;
+
   return (
     <div className="flex-1 bg-slate-100 min-h-screen p-4 md:p-8">
 
@@ -12,25 +29,25 @@ export default function Dashboard() {
 
         <StatCard
           title="Ventas Hoy"
-          value="C$ 5,200"
+          value={`C$ ${ingresos}`}
           color="text-cyan-600"
         />
 
         <StatCard
           title="Facturas"
-          value="25"
+          value={facturas}
           color="text-emerald-600"
         />
 
         <StatCard
           title="Mesas"
-          value="12"
+          value={tables}
           color="text-orange-500"
         />
 
         <StatCard
           title="Productos"
-          value="140"
+          value={initialProducts}
           color="text-violet-600"
         />
 
