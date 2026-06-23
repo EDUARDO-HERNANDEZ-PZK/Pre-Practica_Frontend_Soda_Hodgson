27a17d79e58a6bc52e6b65efed5a9b3ce2432ff8
Eduardo <eherdez196@gmail.com>
2026-06-23 09:47:19 -0600

    cambio de nombre de la tarjeta a apertura de caja "ventas hoy tenia"

--- frontendSodaHodgson/src/pages/Dashboard.jsx
+++ frontendSodaHodgson/src/pages/Dashboard.jsx
@@ -86,7 +86,7 @@ const salesChartData = {
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-6 mt-8">
 
         <StatCard
-          title="Ventas Hoy"
+          title="Apertura de Caja"
           value={`C$ ${ingresos}`}
           color="text-cyan-600"
         />
