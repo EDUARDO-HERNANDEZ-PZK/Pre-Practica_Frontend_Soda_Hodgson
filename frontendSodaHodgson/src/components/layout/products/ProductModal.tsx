6468e5d0f6c761d782ec24093bded1474e8cad56
oscarMolina1523 <oscarmolina1523@gmail.com>
2026-06-24 12:56:03 -0600

    fix: solve error fron cancel button

--- frontendSodaHodgson/src/components/layout/products/ProductModal.tsx
+++ frontendSodaHodgson/src/components/layout/products/ProductModal.tsx
@@ -18,35 +18,35 @@ export default function ProductModal({
 }: Props) {
 
   const { data: categories = [] } = useCategories();
-  
+
   const initialForm: CreateProductDto = {
-  name: "",
-  description: "",
-  category_id: "",
-  unit_id: "",
-  price_sell: 0,
-  stock_current: 0,
-  stock_expired: 0,
-  stock_damaged: 0,
-  stock_min: 0,
-};
+    name: "",
+    description: "",
+    category_id: "",
+    unit_id: "",
+    price_sell: 0,
+    stock_current: 0,
+    stock_expired: 0,
+    stock_damaged: 0,
+    stock_min: 0,
+  };
 
- const [form, setForm] = useState<CreateProductDto>(initialForm);
+  const [form, setForm] = useState<CreateProductDto>(initialForm);
 
-const stockAvailable = Math.max(
-  0,
-  form.stock_current -
+  const stockAvailable = Math.max(
+    0,
+    form.stock_current -
     form.stock_expired -
     form.stock_damaged
-);
+  );
 
-const isFormValid =
-  form.name.trim() !== "" &&
-  form.category_id !== "" &&
-  form.price_sell > 0 &&
-  form.stock_current > 0 &&
-  form.stock_min > 0 &&
-  (form.stock_expired + form.stock_damaged <= form.stock_current);
+  const isFormValid =
+    form.name.trim() !== "" &&
+    form.category_id !== "" &&
+    form.price_sell > 0 &&
+    form.stock_current > 0 &&
+    form.stock_min > 0 &&
+    (form.stock_expired + form.stock_damaged <= form.stock_current);
   const resetForm = () => {
     setForm(initialForm);
   };
@@ -54,16 +54,16 @@ const isFormValid =
   useEffect(() => {
     if (product) {
       setForm({
-    name: product.name,
-    description: product.description,
-    category_id: product.category_id,
-    unit_id: product.unit_id,
-    price_sell: product.price_sell,
-    stock_current: product.stock_current,
-    stock_expired: product.stock_expired,
-    stock_damaged: product.stock_damaged,
-    stock_min: product.stock_min,
-});
+        name: product.name,
+        description: product.description,
+        category_id: product.category_id,
+        unit_id: product.unit_id,
+        price_sell: product.price_sell,
+        stock_current: product.stock_current,
+        stock_expired: product.stock_expired,
+        stock_damaged: product.stock_damaged,
+        stock_min: product.stock_min,
+      });
     } else {
       resetForm();
     }
@@ -164,8 +164,8 @@ const isFormValid =
             className="w-full border rounded-xl p-3"
           />
           <label className="block mb-2 font-medium">
-  Stock total
-</label>
+            Stock total
+          </label>
           <input
             type="number"
             placeholder="Stock total"
@@ -179,97 +179,80 @@ const isFormValid =
             className="w-full border rounded-xl p-3"
           />
 
-<label className="block mb-2 font-medium">
-  Productos vencidos
-</label>
+          <label className="block mb-2 font-medium">
+            Productos vencidos
+          </label>
 
-<input
-  type="number"
-  placeholder="Productos vencidos"
-  value={form.stock_expired || ""}
-  onChange={(e) =>
-    setForm({
-      ...form,
-      stock_expired: e.target.valueAsNumber || 0,
-    })
-  }
-  className="w-full border rounded-xl p-3"
-/>
+          <input
+            type="number"
+            placeholder="Productos vencidos"
+            value={form.stock_expired || ""}
+            onChange={(e) =>
+              setForm({
+                ...form,
+                stock_expired: e.target.valueAsNumber || 0,
+              })
+            }
+            className="w-full border rounded-xl p-3"
+          />
 
-<label className="block mb-2 font-medium">
-  Productos dañados
-</label>
+          <label className="block mb-2 font-medium">
+            Productos dañados
+          </label>
 
-<input
-  type="number"
-  placeholder="Productos dañados"
-  value={form.stock_damaged || ""}
-  onChange={(e) =>
-    setForm({
-      ...form,
-      stock_damaged: e.target.valueAsNumber || 0,
-    })
-  }
-  className="w-full border rounded-xl p-3"
-/>
-<div className="bg-cyan-50 border border-cyan-200 rounded-2xl p-5">
+          <input
+            type="number"
+            placeholder="Productos dañados"
+            value={form.stock_damaged || ""}
+            onChange={(e) =>
+              setForm({
+                ...form,
+                stock_damaged: e.target.valueAsNumber || 0,
+              })
+            }
+            className="w-full border rounded-xl p-3"
+          />
+          <div className="bg-cyan-50 border border-cyan-200 rounded-2xl p-5">
 
-  <p className="text-slate-500 text-sm">
-    Stock disponible
-  </p>
+            <p className="text-slate-500 text-sm">
+              Stock disponible
+            </p>
 
-  <h2 className="text-4xl font-bold text-cyan-700 mt-2">
-    {stockAvailable}
-  </h2>
+            <h2 className="text-4xl font-bold text-cyan-700 mt-2">
+              {stockAvailable}
+            </h2>
 
-</div>
+          </div>
           <label className="block mb-2 font-medium">
             Stock minimo
           </label>
-        <input
-  type="number"
-  placeholder="Stock minimo"
-  value={form.stock_min || ""}
-  onFocus={(e) => {
-    if (e.target.value === "0") {
-      e.target.select();
-    }
-  }}
-  onChange={(e) =>
-    setForm({
-      ...form,
-      stock_min: e.target.valueAsNumber || 0,
-    })
-  }
-  className="w-full border rounded-xl p-3"
-/>
+          <input
+            type="number"
+            placeholder="Stock minimo"
+            value={form.stock_min || ""}
+            onFocus={(e) => {
+              if (e.target.value === "0") {
+                e.target.select();
+              }
+            }}
+            onChange={(e) =>
+              setForm({
+                ...form,
+                stock_min: e.target.valueAsNumber || 0,
+              })
+            }
+            className="w-full border rounded-xl p-3"
+          />
 
         </div>
 
         <div className="flex justify-end gap-3 mt-8">
 
           <button
-        onClick={() => {
-
-  if (
-    form.stock_expired + form.stock_damaged >
-    form.stock_current
-  ) {
-    alert(
-      "Los productos vencidos y dañados no pueden ser mayores al stock total."
-    );
-    return;
-  }
-
-  if (product) {
-    onEditProduct(product.id, form);
-  } else {
-    onSave(form);
-  }
-
-  resetForm();
-  onClose();
-}}
+            onClick={() => {
+              resetForm();
+              onClose();
+            }}
             className="bg-slate-300 px-5 py-3 rounded-xl"
           >
             Cancelar
@@ -278,60 +261,59 @@ const isFormValid =
           <button
             onClick={() => {
 
-  if (!form.name.trim()) {
-    alert("Debe ingresar el nombre del producto");
-    return;
-  }
+              if (!form.name.trim()) {
+                alert("Debe ingresar el nombre del producto");
+                return;
+              }
 
-  if (!form.category_id) {
-    alert("Debe seleccionar una categoría");
-    return;
-  }
+              if (!form.category_id) {
+                alert("Debe seleccionar una categoría");
+                return;
+              }
 
-  if (form.price_sell <= 0) {
-    alert("Debe ingresar un precio válido");
-    return;
-  }
+              if (form.price_sell <= 0) {
+                alert("Debe ingresar un precio válido");
+                return;
+              }
 
-  if (form.stock_current <= 0) {
-    alert("Debe ingresar el stock total");
-    return;
-  }
+              if (form.stock_current <= 0) {
+                alert("Debe ingresar el stock total");
+                return;
+              }
 
-  if (form.stock_min <= 0) {
-    alert("Debe ingresar el stock mínimo");
-    return;
-  }
+              if (form.stock_min <= 0) {
+                alert("Debe ingresar el stock mínimo");
+                return;
+              }
 
-  if (
-    form.stock_expired +
-    form.stock_damaged >
-    form.stock_current
-  ) {
-    alert(
-      "Los productos vencidos y dañados no pueden ser mayores al stock total"
-    );
-    return;
-  }
+              if (
+                form.stock_expired +
+                form.stock_damaged >
+                form.stock_current
+              ) {
+                alert(
+                  "Los productos vencidos y dañados no pueden ser mayores al stock total"
+                );
+                return;
+              }
 
-  if (product) {
-  onEditProduct(product.id, form);
-} else {
-  onSave(form);
-}
+              if (product) {
+                onEditProduct(product.id, form);
+              } else {
+                onSave(form);
+              }
 
-resetForm();
-onClose();
-}}
-className={`px-5 py-3 rounded-xl text-white font-semibold transition ${
-  isFormValid
-    ? "bg-green-600 hover:bg-green-700"
-    : "bg-gray-400 cursor-not-allowed"
-}`}
-disabled={!isFormValid}
->
-  Guardar
-</button>
+              resetForm();
+              onClose();
+            }}
+            className={`px-5 py-3 rounded-xl text-white font-semibold transition ${isFormValid
+                ? "bg-green-600 hover:bg-green-700"
+                : "bg-gray-400 cursor-not-allowed"
+              }`}
+            disabled={!isFormValid}
+          >
+            Guardar
+          </button>
 
         </div>
 
